11d5bfa88d3c28e7c2e69e619c48f6c9c7fac352
AbhijeetX5 <92079531+AbhijeetX5@users.noreply.github.com>
2022-02-09 09:02:19 +0530

    Update Q2.c

--- 4/Q2.c
+++ 4/Q2.c
@@ -2,12 +2,17 @@
 
 int main()
 {
-    int n=10;
-    int arr[n]={23, 10, 34, 23, 90, 10, 34, 23, 56, 49};
+    int n;
+    printf("Array length : ");
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0; i<n; i++){
+        scanf("%d",&arr[i]);
+    }
     int sum=0;
     for(int i=0; i<n; i++){
         sum=sum+arr[i];
     }
-    printf("%d \n",sum);
+    printf("Sum of all elements in the Array is %d. \n",sum);
     return 0;
 }   
